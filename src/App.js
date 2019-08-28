ad9e5cc3b7f020927736e44377ba21b9ad7e20b9
A <avilavate@live.in>
2019-08-28 15:04:01 +0530

    added cards

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+const CardList =(props)=> {
+ return(
+   <div>
+ <Card/>
+ <Card/>
+ <Card />
+ </div>
+ );
+}
+
 class Card extends React.Component {
+
+testData = [
+      {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+      {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+  ];
+  
 	render() {
-  	return (
-    	<div className="github-profile">
-    	  <img src="https://placehold.it/75" />
+    const profile=this.testData[0];
+  	return (<div className="github-profile">
+    	  <img src={ profile.avatar_url } style={{width:'75px'}}/>
         <div className="info">
-          <div className="name">Name here...</div>
-          <div className="company">Company here...</div>
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
         </div>
     	</div>
     );
@@ -21,7 +41,7 @@ class App extends React.Component {
   	return (
     	<div>
     	  <div className="header">The GitHub Cards App</div>
-        <Card />
+        <CardList />
     	</div>
     );
   }	
