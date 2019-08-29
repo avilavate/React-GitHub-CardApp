ce935b7a4c6eb44d2f3a534d4e46144df21fb271
Avil Avate <avilavate123@gmail.com>
2019-08-29 15:43:36 +0530

    Stat obj

--- src/App.js
+++ src/App.js
@@ -1,46 +1,43 @@
 import React from 'react';
 import './App.css';
 
-
-
-
-const CardList =(props)=> {
- return(
-   <div> 
-    {props.testData.map(profile=>{ 
-     return ( <Card {...profile}/>);
-     })} 
- </div>
- );
+const CardList = (props) => {
+  return (
+    <div>
+      {props.testData.map(profile => {
+        return (<Card {...profile} />);
+      })}
+    </div>
+  );
 }
 
 class Card extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.name=props.name;
-    this.avatar_url=props.avatar_url;
-    this.company=props.company;
+    this.name = props.name;
+    this.avatar_url = props.avatar_url;
+    this.company = props.company;
 
   }
 
-  
-	render() {
+
+  render() {
     //const profile=this.testData[0];
-  	return (<div className="github-profile">
-    	  <img src={ this.avatar_url } style={{width:'75px'}}/>
-        <div className="info">
-          <div className="name">{this.name}</div>
-          <div className="company">{this.company}</div>
-        </div>
-    	</div>
+    return (<div className="github-profile">
+      <img src={this.avatar_url} style={{ width: '75px' }} />
+      <div className="info">
+        <div className="name">{this.name}</div>
+        <div className="company">{this.company}</div>
+      </div>
+    </div>
     );
   }
 }
 
-class Form extends React.Component{
-  render(){
-    return(
+class Form extends React.Component {
+  render() {
+    return (
       <form action="">
         <input type="text" placeholder="Github uersmae"></input>
         <button>Add New Card</button>
@@ -51,27 +48,27 @@ class Form extends React.Component{
 
 class App extends React.Component {
   testData = [
-    {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-];
-  
-  constructor(props){
+    { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
+    { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
+    { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
+  ];
+
+  constructor(props) {
     super(props);
-    this.state={
-      testData:this.testData
+    this.state = {
+      testData: this.testData
     }
   }
-  
-	render() {
-  	return (
-    	<div>
-    	  <div className="header">The GitHub Cards App</div>
+
+  render() {
+    return (
+      <div>
+        <div className="header">The GitHub Cards App</div>
         <Form></Form>
-        <CardList  testData={this.state.testData}/>
-    	</div>
+        <CardList testData={this.state.testData} />
+      </div>
     );
-  }	
+  }
 }
 
 export default App;
